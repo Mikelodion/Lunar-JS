882a9c9c96afa1275ae3dca51e01adf7b0cc0904
Mikelodion <32365541+Mikelodion@users.noreply.github.com>
2018-01-15 12:40:59 +0100

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -11,8 +11,13 @@ var c = 100;
 var a = g; //la aceleración cambia cuando se enciende el motor de a=g a a=-g (simplificado)
 //MARCADORES
 var velocidad = null;
-var altura = null;
+var altur= null;
 var combustible = null;
+var menuActivo = true;
+//Dificultad
+var facil = false;
+var normal=false;
+var dificil=false;
 
 //al cargar por completo la página...
 window.onload = function(){
@@ -26,19 +31,50 @@ window.onload = function(){
 	//mostrar menú móvil
     	document.getElementById("engranaje").onclick = function () {
 		document.getElementById("menu").style.display = "inline-block";
+		menuActivo = true;
 		stop();
 	}
 	//ocultar menú móvil
 	document.getElementById("return").onclick = function () {
 		document.getElementById("menu").style.display = "none";
+		menuActivo = false;
 		start();
 	}
+	//CAmbiar dificultad
+		document.getElementsByClassName("button")[0].onclick = function () {
+			facil=true;
+			normal=false;
+			dificil=false;
+			cambiarDificultad();
+		}
+		document.getElementsByClassName("button")[1].onclick = function () {
+			facil=false;
+			normal=true;
+			dificil=false;
+			cambiarDificultad();
+		}
+		document.getElementsByClassName("button")[2].onclick = function () {
+			facil=false;
+			normal=false;
+			dificil=true;
+			cambiarDificultad();
+		}
+
+
+	//encender/apagar el motor al hacer click en la pantalla
+	document.onclick = function () {
+ 	  if (a==g){
+  		motorOn();
+ 	  } else {
+  		motorOff();
+ 	  }
+	}
 	//encender/apagar al apretar/soltar una tecla
+	
 	document.onkeydown = motorOn;
 	document.onkeyup = motorOff;
-	
 	//Empezar a mover la nave justo después de cargar la página
-	start();
+	
 }
 
 //Definición de funciones
@@ -49,15 +85,31 @@ function start(){
 
 function stop(){
 	clearInterval(timer);
+	motorOff();
 }
 
 function moverNave(){
 	//cambiar velocidad y posicion
 	v +=a*dt;
 	y +=v*dt;
+	altur=70-y;
 	//actualizar marcadores
-	velocidad.innerHTML=v.toFixed(2);
-	altura.innerHTML=y.toFixed(0);
+	if (altur<0 || v>0) {
+		if (v>0) {
+		velocidad.innerHTML=v.toFixed(2);
+		}
+		if (altur<0) {
+			v=0;
+			velocidad.innerHTML=v.toFixed(2);
+			altura.innerHTML=-altur.toFixed(0);
+		}
+	}
+	else{
+		velocidad.innerHTML=-v.toFixed(2);
+		altura.innerHTML=altur.toFixed(0);
+	}
+
+	
 	
 	//mover hasta que top sea un 70% de la pantalla
 	if (y<70){ 
@@ -69,13 +121,14 @@ function moverNave(){
 }
 function motorOn(){
 	//el motor da aceleración a la nave
+	if (!c==0&&y<70&&!menuActivo) {
 	a=-g;
-	document.getElementById("nave").style.display= "none";
+	document.getElementById("nave").style.display= "none";	
 	document.getElementById("nave1").style.display="inline-block";
 	//mientras el motor esté activado gasta combustible
 	if (timerFuel==null)
 	timerFuel=setInterval(function(){ actualizarFuel(); }, 10);
-
+}
 }
 function motorOff(){
 	a=g;
@@ -87,6 +140,21 @@ function motorOff(){
 function actualizarFuel(){
 	//Restamos combustible hasta que se agota
 	c-=0.1;
-	if (c < 0 ) c = 0;
-	combustible.innerHTML=c.toFixed(0);	
+	if (c < 0 ) {
+		c = 0;
+		motorOff();
+	}
+	combustible.innerHTML=c.toFixed(0);
+}
+function cambiarDificultad(){
+	
+	if (facil) {
+		c=100;
+	}
+	if (normal){
+		c=80;
+	}
+	if (dificil) {
+		c=60;
+	}
 }
